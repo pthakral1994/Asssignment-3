ffb34339da5e7c9c3305509aecde7713d405d1ab
pthakral1994 <pthakral3781@conestogac.on.ca>
2024-03-18 15:08:59 -0400

    Adding class file

---
+++ Asssignment 3/motorcycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_vehicle
+{
+    public class Motorcycle : Vehicle
+    {
+        public int EngineCapacity { get; set; }
+        public string FuelType { get; set; }
+        public bool HasFairing { get; set; }
+
+        public Motorcycle(string model, string manufacturer, int year, decimal rentalPrice, int engineCapacity, string fuelType, bool hasFairing)
+            : base(model, manufacturer, year, rentalPrice)
+        {
+            EngineCapacity = engineCapacity;
+            FuelType = fuelType;
+            HasFairing = hasFairing;
+        }
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Engine Capacity: {EngineCapacity}");
+            Console.WriteLine($"Fuel Type: {FuelType}");
+            Console.WriteLine($"Has Fairing: {HasFairing}");
+        }
+    }
+}
