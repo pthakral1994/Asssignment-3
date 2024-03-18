5ba77ab65b784406ac2fb05de1674320a552d7c1
pthakral1994 <pthakral3781@conestogac.on.ca>
2024-03-18 15:06:03 -0400

    Adding classs file

---
+++ Asssignment 3/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_vehicle
+{
+    public class Car : Vehicle
+    {
+        public int Seats { get; set; }
+        public string EngineType { get; set; }
+        public string Transmission { get; set; }
+        public bool Convertible { get; set; }
+
+        public Car(string model, string manufacturer, int year, decimal rentalPrice, int seats, string engineType, string transmission, bool convertible)
+            : base(model, manufacturer, year, rentalPrice)
+        {
+            Seats = seats;
+            EngineType = engineType;
+            Transmission = transmission;
+            Convertible = convertible;
+        }
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Seats: {Seats}");
+            Console.WriteLine($"Engine Type: {EngineType}");
+            Console.WriteLine($"Transmission: {Transmission}");
+            Console.WriteLine($"Convertible: {Convertible}");
+        }
+    }
+}
